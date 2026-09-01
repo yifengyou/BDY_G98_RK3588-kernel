0eca4470418b7aea26e2c1a5920fb9f15a1a1186
yifengyou <842056007@qq.com>
2026-09-01 10:31:50 +0800

    缺陷修复,压缩体积
    增加openssh-sftp包，避免多线程下载错误
    压缩rkdev工具体积
    
    Signed-off-by: yifengyou <842056007@qq.com>

--- rkdev/build.sh
+++ rkdev/build.sh
@@ -14,6 +14,16 @@ go version
 
 go mod tidy
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -trimpath -ldflags="-s -w" -o rkdev_arm64 .
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-s -w" -o rkdev_amd64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build \
+  -trimpath \
+  -ldflags="-s -w -extldflags=-static" \
+  -gcflags="all=-l -B" \
+  -o rkdev_arm64 .
 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build \
+  -trimpath \
+  -ldflags="-s -w -extldflags=-static" \
+  -gcflags="all=-l -B" \
+  -o rkdev_amd64 .
+
+ls -al rkdev_amd64 rkdev_arm64
